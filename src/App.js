14bfad103f41bc9be84e77a1fb149cf57a04b0af
gergokutu <gergo.kutu@gmail.com>
2019-08-01 12:32:56 +0200

    for route testin »src/components/Home.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import store from './store'
+import { Provider } from 'react-redux'
+import { Route } from 'react-router-dom'
+import Home from './components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Events-manager</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <Route path="/" exact component={Home} />
+        </div>
+      </Provider>
+    );
+  }
 }
 
-export default App;
+export default App
